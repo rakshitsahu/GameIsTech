c30d3bb234b77a588ccb7baa14543a928a743dc9
Rakshit <rakshitsahu20@gmail.com>
2023-04-18 01:34:45 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,19 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <div className="flex flex-row place-items-center bg-neutral-800 ">
-        <nav className="flex flex-row  bg-neutral-800 rounded-b-md">
+    <div className=" flex justify-between bg-neutral-800">
+    <span className="flex flex-row  bg-neutral-800">
+    <Image
+        src="/coder.png"
+        alt="Picture of the author"
+        width={250}
+        height={250}
+      />
+    </span>
+    </div>
+      <div className="flex justify-center bg-red-800 ">
+      
+        <nav className="flex bg-neutral-800 rounded-b-md">
 
           <span className="p-6 bg-gradient-to-r hover:bg-red-500 ml-4 delay-200 ">
             <font className="font-semibold text-xl text-white">Home</font>

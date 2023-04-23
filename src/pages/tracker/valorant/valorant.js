1cd12377f2c5daf133969c5edb2e863046278fd9
Rakshit <rakshitsahu20@gmail.com>
2023-04-23 18:26:34 +0530

    valo Homepage

--- src/pages/tracker/valorant/valorant.js
+++ src/pages/tracker/valorant/valorant.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect , useState } from 'react'
 import {GiTrophyCup,GiTrapMask} from 'react-icons/gi'
 import {TbMapSearch} from 'react-icons/tb'
 import {IoSparklesSharp} from 'react-icons/io5'
-const valorant = () => {
+import axios from 'axios'
+
+export default function Valorant() {
+  const [Agents , setAgents] = useState({})
+  const getdata = async ()=>{
+    try {
+      const res = await axios.get('https://valorant-api.com/v1/agents');
+      setAgents(res.data.data)
+      console.log(Agents.length)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    getdata()
+  },[])
+  
   return (
     <>
     <nav className='flex flex-wrap w-full bg-gray-700 text-white p-5 gap-3'>
@@ -11,9 +27,27 @@ const valorant = () => {
     <span className='flex gap-2'> <IoSparklesSharp className='self-center '/>Skins</span>
     <span className='flex gap-2'> <GiTrophyCup className='self-center '/> Leaderboard </span>
     </nav>
+    <div className="grid grid-cols-2 gap-3 mt-4 p-4">
     
+    <span className='flex flex-row  p-7 rounded-lg '>    <div className='flex flex-wrap gap-3 justify-center'>
+    <div className='text-white w-full  text-xl'> <center>Agents</center> </div>
+  
+    {Object.keys(Agents).map(  (Agent) => {
+      return (
+        <div className='rounded-full flex-wrap shadow-2xl border-2 border-black' key={Agent}>
+        { Agents[Agent].isPlayableCharacter && <div className='h-24 w-24 rounded-full shadow-2xl overflow-clip bg-red-500' key={Agent}>
+        <img src={Agents[Agent].fullPortraitV2} className='self-auto overflow-clip shadow-2xl' alt="" />
+        
+        </div> }
+        </div>
+      );
+    })}
+</div></span>
+    <span className='flex flex-row bg-blue-600'>Block 2</span>
+    <span className='flex flex-row bg-blue-600'>Block 3</span>
+    <span className='flex flex-row bg-blue-600'>Block 4</span>
+    </div>
+
     </>
   )
 }
-
-export default valorant

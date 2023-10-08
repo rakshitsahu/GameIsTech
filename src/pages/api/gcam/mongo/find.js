75d484135abd95a6ea7b3d75aa6eecf7a8a2a0bc
rakshit <rakshitsahu20@gmail.com>
2023-10-08 23:54:18 +0530

    done

--- src/pages/api/gcam/mongo/find.js
+++ src/pages/api/gcam/mongo/find.js
@@ -5,16 +5,15 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = "mongodb+srv://admin1:admin@cluster0.eejo5yk.mongodb.net/?retryWrites=true&w=majority";
 async function MongoFind(req , res){
-  // console.log( 'this is find api', req.body);
+  console.log( 'db name is', process.env.GCAM_DB_NAME);
   // console.log('the type of req body is', typeof req.body);
   const body = req.body
   // console.log('the request body is', body)
 
     const collection = req.body.collection
     const filter = req.body.filter
-    console.log(' collection is', collection)
-    console.log('filter is', filter)
-    console.log('type of body is', typeof req.body)
+    // console.log(' collection is', collection)
+    // console.log('filter is', filter)
 
 
     const client = new MongoClient(uri, {
@@ -26,20 +25,17 @@ async function MongoFind(req , res){
       });
       try {
         await client.connect().catch( async (err) => { 
-          console.log('the error occurred is', err)
+          // console.log('the error occurred is', err)
           await client.close() 
         }
-           ).then( (result) =>{
-            console.log('database has been successfully conected')
-           })
+           )
         // console.log('request has been fetched successfully')
         const data = await client.db(process.env.GCAM_DB_NAME).collection(collection).find(filter).toArray();
-        console.log('request has been fetched successfully', data)
         await client.close()
         res.send(data)
       } catch (error) {
         // console.log('error fetching data' , error)
-        console.log('the error occurred is', error)
+
         await client.close()
       }
 

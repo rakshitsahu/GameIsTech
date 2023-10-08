7006b7d9b0635f3e1826bfb39ebfe7dea2198676
rakshit <rakshitsahu20@gmail.com>
2023-10-08 14:21:56 +0530

    redirection removed

--- app.js
+++ app.js
@@ -1,54 +1,40 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
-const axios = require('axios');
-
-const dev = process.env.NODE_ENV !== 'production';
-const host = 'localhost'; // Change the host to localhost
-const port = process.env.PORT ;
-console.log("host name is", host , port)
-const callApi = async () => {
-  try {
-    await axios.get('http://localhost:3000/api/hello');
-  } catch (error) {
-    console.error('API call failed:', error.message);
-  }
-};
-
-const app = next({ dev, host, port });
-const handle = app.getRequestHandler();
-
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+ 
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = process.env.PORT
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
+ 
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
-      const parsedUrl = parse(req.url, true);
-      const { pathname, query } = parsedUrl;
-
-      const forwardedHost = req.headers['x-forwarded-host'];
-
-      if (
-        forwardedHost === 'www.apkhub.mobi'
-      ) {
-        // Redirect to https://apkhub.mobi
-        res.writeHead(301, { Location: `https://apkhub.mobi${req.url}` });
-        res.end();
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true)
+      const { pathname, query } = parsedUrl
+ 
+      if (pathname === '/a') {
+        await app.render(req, res, '/a', query)
+      } else if (pathname === '/b') {
+        await app.render(req, res, '/b', query)
       } else {
-        // Handle other cases
-        parsedUrl.protocol = 'https';
-        await handle(req, res, parsedUrl);
+        await handle(req, res, parsedUrl)
       }
     } catch (err) {
-      console.error('Error occurred handling', req.url, err);
-      res.statusCode = 500;
-      res.end('Internal server error');
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
     }
   })
     .once('error', (err) => {
-      console.error(err);
-      process.exit(1);
+      console.error(err)
+      process.exit(1)
     })
-    .listen(port, host, () => {
-      console.log(`> Ready on http://${host}:${port}`);
-      setInterval(callApi, 3600000);
-    });
-});
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`)
+    })
+})

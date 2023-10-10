f9fad3d208ccdc1fc49ff5f108e12014c27c91a2
rakshit <rakshitsahu20@gmail.com>
2023-10-10 23:29:39 +0530

    analytiics remmoved

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,8 @@
 import '@/styles/globals.css'
 import NextNProgress from 'nextjs-progressbar';
-
 export default function App({ Component, pageProps }) {
   
   return<>
-
-
   <NextNProgress />
 <Component {...pageProps} />
 </> 

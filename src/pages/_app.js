8c5413ed179f9e7303785173fc7adc2dba09aaf2
rakshit <rakshitsahu20@gmail.com>
2023-10-10 23:23:23 +0530

    analytics removed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,10 @@
 import '@/styles/globals.css'
 import NextNProgress from 'nextjs-progressbar';
-import Script from 'next/script';
+
 export default function App({ Component, pageProps }) {
   
   return<>
-  <div className="container">
-  <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`} />
-  <Script strategy="lazyOnload" id="google-analytics">
-    {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
 
-      gtag('config', '${process.env.GOOGLE_ANALYTICS}');
-    `}
-  </Script>
-</div>
 
   <NextNProgress />
 <Component {...pageProps} />

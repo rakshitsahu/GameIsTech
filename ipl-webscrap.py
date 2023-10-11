96e6e8c830bdb23f297de51e4ce5b98697f2d926
rakshit <rakshitsahu20@gmail.com>
2023-10-11 21:56:42 +0530

    analytics tag added

--- ipl-webscrap.py
+++ ipl-webscrap.py
@@ -1,8 +1,5 @@
-import requests
 from bs4 import BeautifulSoup
 import time
-
-# Send an HTTP GET request to the URL
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,13 +10,15 @@ driver = webdriver.Chrome()
 
 # Define the URL you want to scrape
 url = "https://www.iplt20.com/match/2023/1046"
+waitTillLastClass = "recentBallsList"
+teamBattleCssClass = "ap-teams-battle-wrp"
 
 # Load the page
 driver.get(url)
 
 # Wait for the page to fully load (you can adjust the timeout as needed)
 wait = WebDriverWait(driver, 10)
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, "your-element-class")))  # Replace "your-element-class" with the actual element you want to wait for
+wait.until(EC.presence_of_element_located((By.CLASS_NAME, waitTillLastClass)))
 
 # Get the HTML content of the loaded page
 html_content = driver.page_source
@@ -27,8 +26,13 @@ html_content = driver.page_source
 # Close the WebDriver
 driver.quit()
 
-# Now you can parse the HTML content using BeautifulSoup or any other method
-soup = BeautifulSoup(html_content, 'html.parser')
-print(soup)
-with open("ipldata.txt", "a") as f:
-        print(soup, file=f)
+# Replace problematic characters with a placeholder (e.g., '?')
+html_content_cleaned = html_content.encode('utf-8', 'replace').decode('utf-8')
+
+# Now you can parse the cleaned HTML content using BeautifulSoup or any other method
+soup = BeautifulSoup(html_content_cleaned, 'html.parser')
+battleSection = soup.find('div', attrs={'class':teamBattleCssClass})
+# Write the cleaned HTML content to a file with UTF-8 encoding
+# print(battleSection)
+with open("ipldata.txt", "a", encoding="utf-8") as f:
+    f.write(str(battleSection))
